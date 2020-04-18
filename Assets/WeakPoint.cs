bd03f8493abda5369f85eb39f9830b9989ab4bcb
swanickj <swanickj@tipmedia.com>
2020-04-18 14:26:47 -0400

    spears and planet flinching

---
+++ Assets/WeakPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPoint : MonoBehaviour
+{
+    public Planet myplanet;
+    public Vector3 finalScaleAfterHit = Vector3.forward;
+    public void TakeHit() {
+        myplanet.TakeDamage();
+        StartCoroutine(WeakPointScale());
+        GetComponent<Collider>().enabled = false;
+    }
+
+    public AnimationCurve WeakPointScaleCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+    public float WeakPointScaleTime = 1f;
+    private IEnumerator WeakPointScale() {
+        float currTime = 0f;
+        float lerpVal;
+        WaitForEndOfFrame wfeof = new WaitForEndOfFrame();
+        Vector3 iscale = transform.localScale;
+        while (currTime < WeakPointScaleTime) {
+            currTime += Time.deltaTime;
+            lerpVal = WeakPointScaleCurve.Evaluate(Mathf.InverseLerp(0f, WeakPointScaleTime, currTime));
+            transform.localScale = Vector3.Lerp(iscale, finalScaleAfterHit, lerpVal);
+            yield return wfeof;
+        }
+    }
+}
