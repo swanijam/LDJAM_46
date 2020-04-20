887ae584c0b79c56aa20ff46f8460db52f61aa77
swanickj <swanickj@tipmedia.com>
2020-04-20 18:05:37 -0400

    proc anim WIP

---
+++ Assets/straightArmAttach.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class straightArmAttach : MonoBehaviour
+{
+    public Transform shoulder;
+    public Transform hand;
+    public Transform grabTarget;
+    public Transform elbow;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        shoulder.LookAt(grabTarget);
+        hand.position = grabTarget.position;
+        elbow.position = Vector3.Lerp(shoulder.position, hand.position, .5f);
+    }
+}
