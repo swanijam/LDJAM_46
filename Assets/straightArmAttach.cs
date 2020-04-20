686ace1410617988aa9a44c1f457e13facfea24f
swanickj <swanickj@tipmedia.com>
2020-04-20 18:20:02 -0400

    event null check

--- Assets/straightArmAttach.cs
+++ Assets/straightArmAttach.cs
@@ -6,6 +6,7 @@ public class straightArmAttach : MonoBehaviour
 {
     public Transform shoulder;
     public Quaternion iShoulderRot;
+    public Quaternion iShoulderRelative;
     public Vector3 iHandPos;
     public Transform hand;
     public Transform grabTarget;
@@ -14,14 +15,22 @@ public class straightArmAttach : MonoBehaviour
     private void Awake()
     {
         iShoulderRot = shoulder.localRotation;
+        iShoulderRelative = Quaternion.FromToRotation(hand.position-shoulder.position, shoulder.forward);
         iHandPos = hand.localPosition;
         iElbowPos = hand.localPosition;
     }
+    public bool grabbing = false;
     // Update is called once per frame
     void Update()
     {
-        shoulder.LookAt(grabTarget);
-        hand.position = grabTarget.position;
-        elbow.position = Vector3.Lerp(shoulder.position, hand.position, .5f);
+        if (grabbing) {
+            shoulder.rotation = Quaternion.LookRotation(grabTarget.position - shoulder.position) * iShoulderRelative;
+            hand.position = grabTarget.position;
+            elbow.position = Vector3.Lerp(shoulder.position, hand.position, .5f);
+        } else {
+            shoulder.localRotation = iShoulderRot;
+            hand.localPosition = iHandPos;
+            elbow.localPosition = iElbowPos;
+        }
     }
 }
