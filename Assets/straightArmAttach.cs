e240d5575aa17af2a72e9cfa61bd4429a921a9ca
swanickj <swanickj@tipmedia.com>
2020-04-20 18:13:51 -0400

    proc anim WIP

--- Assets/straightArmAttach.cs
+++ Assets/straightArmAttach.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class straightArmAttach : MonoBehaviour
 {
     public Transform shoulder;
+    public Quaternion iShoulderRot;
+    public Vector3 iHandPos;
     public Transform hand;
     public Transform grabTarget;
     public Transform elbow;
-    
+    public Vector3 iElbowPos;
+    private void Awake()
+    {
+        iShoulderRot = shoulder.localRotation;
+        iHandPos = hand.localPosition;
+        iElbowPos = hand.localPosition;
+    }
     // Update is called once per frame
     void Update()
     {
