588f68a43223dd72d49fdd6c439d58e4484a8df8
Matthew Wagar <mrw77@drexel.edu>
2020-04-23 18:04:42 -0700

    Heart Shatter Animation

--- Assets/_SCRIPTS/WeakPoint.cs
+++ Assets/_SCRIPTS/WeakPoint.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class WeakPoint : MonoBehaviour
 {
     public Planet myplanet;
-    public Vector3 finalScaleAfterHit = Vector3.forward;
+    // public Vector3 finalScaleAfterHit = Vector3.forward;
+    public Animator HeartAnimator;
     public void TakeHit() {
         myplanet.TakeDamage();
-        StartCoroutine(WeakPointScale());
+        // StartCoroutine(WeakPointScale());
+        HeartAnimator.SetTrigger("Shatter");
         GetComponent<Collider>().enabled = false;
     }
 
-    public AnimationCurve WeakPointScaleCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
-    public float WeakPointScaleTime = 1f;
-    private IEnumerator WeakPointScale() {
-        float currTime = 0f;
-        float lerpVal;
-        WaitForEndOfFrame wfeof = new WaitForEndOfFrame();
-        Vector3 iscale = transform.localScale;
-        while (currTime < WeakPointScaleTime) {
-            currTime += Time.deltaTime;
-            lerpVal = WeakPointScaleCurve.Evaluate(Mathf.InverseLerp(0f, WeakPointScaleTime, currTime));
-            transform.localScale = Vector3.Lerp(iscale, finalScaleAfterHit, lerpVal);
-            yield return wfeof;
-        }
-    }
+    // public AnimationCurve WeakPointScaleCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+    // public float WeakPointScaleTime = 1f;
+    // private IEnumerator WeakPointScale() {
+    //     float currTime = 0f;
+    //     float lerpVal;
+    //     WaitForEndOfFrame wfeof = new WaitForEndOfFrame();
+    //     Vector3 iscale = transform.localScale;
+    //     while (currTime < WeakPointScaleTime) {
+    //         currTime += Time.deltaTime;
+    //         lerpVal = WeakPointScaleCurve.Evaluate(Mathf.InverseLerp(0f, WeakPointScaleTime, currTime));
+    //         transform.localScale = Vector3.Lerp(iscale, finalScaleAfterHit, lerpVal);
+    //         yield return wfeof;
+    //     }
+    // }
 }
